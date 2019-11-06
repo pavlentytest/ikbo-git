804c6ebc2cbcc7d186ab0ef02c6405452e56fad7
Gaich <dashapereg@mail.ru>
2019-11-06 14:19:04 +0300

    Stackoverflow

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    public static void helpme() {
+        while(true) {
+            Scanner.out.println("Help me!");
+        }
+
+    }
+
 }
