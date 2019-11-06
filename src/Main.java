88091ef759cdb3a4d1e6ba0a9c2da2a8c5e67906
pavlenty <impetus@yandex.ru>
2019-11-06 14:26:20 +0300

    Убрал строку с Маркеловым

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Markelov");
+        
     }
     public static void helpme() {
         while(true) {
             System.out.println("Help me!");
         }
+
 
     }
 
