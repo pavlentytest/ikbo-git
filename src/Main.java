0aade2be1869c57f4e747139f1145c808dc2cd26
naira <nairo_diro@mail.ru>
2019-11-06 14:35:04 +0300

    Opa opa ggg

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Opa opa");
+        ///lkhnkjgjk
     }
     public static void helpme() {
         while(true) {
