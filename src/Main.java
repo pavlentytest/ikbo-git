91a8636fdb003e930644bdd618ebf0e3b88bf3a9
ada32sdf <adadad@gmail.com>
2019-11-06 14:36:01 +0300

    i am

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     }
     public static void helpme() {
         while(true) {
-            System.out.println("Help me1111111111111111111111!");
+            System.out.println("11111111111Help me1111111111111111111111!");
         }
 
     }
