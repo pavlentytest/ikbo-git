a572af022d07e6a7ece1d5ef84427a7d6ef15ac4
BB <b.bar1enev@gmail.com>
2019-11-06 14:38:12 +0300

    wutface

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Opa opa");
-        ///lkhnkjgjk
+        System.out.println("QWE");
     }
-    public static void helpme() {
-        while(true) {
-            System.out.println("lohi");
-        }
-
-    }
-
 }
