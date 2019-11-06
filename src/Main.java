3b31a76e6ed2088fdc250adfc706a932e4d9bb49
Gaya Peregudova <33051130+Tourism-club-Traceintrace-school1576@users.noreply.github.com>
2019-11-06 14:21:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     }
     public static void helpme() {
         while(true) {
-            Scanner.out.println("Help me!");
+            System.out.println("Help me!");
         }
 
     }
